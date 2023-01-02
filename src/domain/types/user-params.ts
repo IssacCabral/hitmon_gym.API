5b06554957f8aabe3c52e1f983f8555fcca59ae4
Issac Cabral <issaccabrall@gmail.com>
2023-01-02 11:17:03 -0300

    add console log

--- src/domain/types/user-params.ts
+++ src/domain/types/user-params.ts
@@ -2,3 +2,5 @@ import { IUser } from '../entities/user';
 
 export type CreateUserParams = Omit<IUser, 'id' | 'createdAt' | 'updatedAt'>;
 export type CreateUserReturns = {};
+
+console.log('Ola amigo');
