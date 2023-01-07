4271fc239cadd301594d2618406dc39b869b233a
Issac Cabral <issaccabrall@gmail.com>
2023-01-07 18:19:17 -0300

    test: Should updateUser to have been called

--- __tests__/unit/data/usecases/auth/db.create-token-reset-password.spec.ts
+++ __tests__/unit/data/usecases/auth/db.create-token-reset-password.spec.ts
@@ -87,4 +87,16 @@ describe('# UseCase - create token reset password', () => {
     const promise = usecase.execute('any_email');
     await expect(promise).rejects.toThrow();
   });
+
+  it('Should updateUser to have been called', async () => {
+    const { usecase, repository } = makeSut();
+
+    jest.spyOn(repository, 'findUserByEmail').mockResolvedValueOnce(userMock);
+
+    const updateUserSpy = jest.spyOn(repository, 'updateUser');
+
+    await usecase.execute('any_email');
+
+    expect(updateUserSpy).toHaveBeenCalled();
+  });
 });
