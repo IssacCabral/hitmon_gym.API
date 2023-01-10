d1a8184886a15e9dde13ecfa584cff3acb5463f0
Issac Cabral <issaccabrall@gmail.com>
2023-01-10 09:40:57 -0300

    fix: make BcryptHashAdapter implements compareHash method

--- src/infra/protocols/hash/bcrypt.hash.adapter.ts
+++ src/infra/protocols/hash/bcrypt.hash.adapter.ts
@@ -4,6 +4,9 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class BcryptHashAdapter implements IHash {
+  compareHash(value: string, hashedValue: string): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
   async generateHash(value: string): Promise<string> {
     return await bcrypt.hash(value, 12);
   }
