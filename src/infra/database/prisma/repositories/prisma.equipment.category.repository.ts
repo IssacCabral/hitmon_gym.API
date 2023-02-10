1875dbefcf28286630d2c65910f56c8779cdb6bc
Issac Cabral <issaccabrall@gmail.com>
2023-02-10 09:14:23 -0300

    feat: add EquipmentCategoryRepository as Prisma service

---
+++ src/infra/database/prisma/repositories/prisma.equipment.category.repository.ts
@@ -0,0 +1,30 @@
+import { IEquipmentCategoryRepository } from '@data/repositories/equipment-category-repository';
+import { IEquipmentCategory } from '@domain/entities/equipment-category';
+import { CreateEquipmentCategoryParams } from '@domain/types/equipment-category-params';
+import { Injectable } from '@nestjs/common';
+import { EquipmentCategory } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class EquipmentCategoryRepository
+  implements IEquipmentCategoryRepository
+{
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createEquipmentCategory(
+    equipmentCategory: CreateEquipmentCategoryParams,
+  ): Promise<IEquipmentCategory> {
+    const createdEquipmentCategory =
+      await this.prismaService.equipmentCategory.create({
+        data: equipmentCategory,
+      });
+
+    return createdEquipmentCategory;
+  }
+
+  async findEquipmentCategoryByName(name: string): Promise<EquipmentCategory> {
+    return await this.prismaService.equipmentCategory.findUnique({
+      where: { name },
+    });
+  }
+}
